91e35874f87aee946b2e5dd7c041d57f69b71636
lgh <2524398864@qq.com>
2022-11-09 15:18:13 +0800

    feat: 完成面试题10，并通过初步测试

---
+++ the_offers_10/the_offers_10_1.cpp
@@ -0,0 +1,35 @@
+﻿/*
+*  题目：求斐波那契数列的第n项
+*/
+
+
+#include<iostream>
+using namespace std;
+
+long long Fibonacci(unsigned n)
+{
+	int result[2] = { 0, 1 };
+
+	if (n < 2)
+		return result[n];
+
+	long long fibNMinusOne = 1;
+	long long fibNMinusTwo = 0;
+	long long fibN = 0;
+
+	for (unsigned int i = 2; i <= n; ++i)
+	{
+		fibN = fibNMinusOne + fibNMinusTwo;
+
+		fibNMinusTwo = fibNMinusOne;
+		fibNMinusOne = fibN;
+	}
+
+	return fibN;
+}
+
+int main(int argc, char* argv)
+{
+	cout << Fibonacci(100) << endl;
+}
+
