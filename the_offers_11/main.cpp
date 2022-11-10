b5610eb5c804b80eb886f1f0a722caabbea07940
lgh <2524398864@qq.com>
2022-11-10 11:50:38 +0800

    fix: 补充边界情况

--- the_offers_11/main.cpp
+++ the_offers_11/main.cpp
@@ -26,12 +26,29 @@ int Min(int* numbers, int length)
 		}
 
 		indexMid = (index1 + index2) / 2;
+
+		// 如果下标为index1、index2和indexMid指向三个数字相同
+		// 则只能按照顺序查找
+		if (numbers[index1] == numbers[index2] && numbers[indexMid] == numbers[index1])
+			return MinInOrder(numbers, index1, index2);
+
 		if (numbers[indexMid] >= numbers[index1])
 			index1 = indexMid;
 		else if (numbers[indexMid] <= numbers[index2])
 			index2 = indexMid;
 	}
 	return numbers[indexMid];
+}
+
+int MinInOrder(int* numbers, int index1, int index2)
+{
+	int result = numbers[index1];
+	for (int i = index1 + 1; i < index2; ++i)
+	{
+		if (result > numbers[i])
+			result = numbers[i];
+	}
+	return result;
 }
 
 int main(int argc, char* argv)
