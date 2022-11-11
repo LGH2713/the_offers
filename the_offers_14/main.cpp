c2a9da76fca1e0658d54663ce07c952a256ab5c9
lgh <2524398864@qq.com>
2022-11-11 17:27:20 +0800

    comment: 增加注释

--- the_offers_14/main.cpp
+++ the_offers_14/main.cpp
@@ -11,13 +11,14 @@ using namespace std;
 //================  动态规划  ====================
 int maxProductAfterCutting_solution1(int length)
 {
-	if (length < 2)
+	if (length < 2)		// 无法分割
 		return 0;
-	if (length == 2)
+	if (length == 2)	// 只能分割为1 1
 		return 1;
-	if (length == 3)
+	if (length == 3)	// 最大分割为1 2
 		return 2;
 
+	// 动规数组
 	int* products = new int[length + 1];
 	products[0] = 0;
 	products[1] = 1;
@@ -25,11 +26,14 @@ int maxProductAfterCutting_solution1(int length)
 	products[3] = 3;
 
 	int max = 0;
+
+	// 每一步的最大值都是基于上一次的结果
 	for (int i = 4; i <= length; ++i)
 	{
 		max = 0;
 		for (int j = 1; j <= i / 2; ++j)
 		{
+			// 计算出当前位的值
 			int product = products[j] * products[i - j];
 			if (max < product)
 				max = product;
@@ -55,19 +59,22 @@ int maxProductAfterCutting_solution2(int length)
 	if (length == 3)
 		return 2;
 
+	// 尽可能剪长度3
 	int timesOf3 = length / 3;
 
 	if (length - timesOf3 * 3 == 1)
 		timesOf3 -= 1;
 
+	// 不能减长度3的就尽可能剪长度2
 	int timesOf2 = (length - timesOf3 * 3) / 2;
 
+	// 计算出最大乘积
 	return static_cast<double>(pow(3, timesOf3)) * static_cast<int>(pow(2, timesOf2));
 }
 
 
 int main(int argc, char* argv)
 {
-	cout << maxProductAfterCutting_solution1(50) << endl;
-	cout << maxProductAfterCutting_solution2(50) << endl;
+	cout << maxProductAfterCutting_solution1(8) << endl;
+	cout << maxProductAfterCutting_solution2(8) << endl;
 }
