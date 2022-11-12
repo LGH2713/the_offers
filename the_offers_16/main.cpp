37b15274336b2f06da90b5405430e39579e57ed4
lgh <2524398864@qq.com>
2022-11-12 10:54:43 +0800

    comment: 完成进阶解法，通过测试，补充注释

--- the_offers_16/main.cpp
+++ the_offers_16/main.cpp
@@ -1,7 +1,7 @@
 /*
 *	题目：实现函数 double Power(double base, int exponent)，求base的exponent次方，同时不需要考虑大数问题
 * 
-*	tips：需要考虑指数小于1（负数和0）的情况
+*	tips：需要考虑指数小于1（负数和0）的情况，使用全局变量判断输入是否非法
 */
 
 #include<iostream>
@@ -19,14 +19,14 @@ bool equal(double a, double b)
 	return false;
 }
 
-// 普通解法：当
-
 bool g_InvalidInput = false;
 
 double Power(double base, int exponent)
 {
+	// 全局标识符重置
 	g_InvalidInput = false;
 
+	// 0的0次方在数学上是没有意义的，所以返回1或0都可以（具体看面试官要求）
 	if (equal(base, 0.0) && exponent < 0)
 	{
 		g_InvalidInput = true;
@@ -44,10 +44,29 @@ double Power(double base, int exponent)
 	return result;
 }
 
-double PowerWithUnsignedExponent(double base, unsigned int exponent)
+// 普通解法
+/*
+double powerwithunsignedexponent(double base, unsigned int exponent)
 {
 	double result = 1.0;
 	for (int i = 1; i <= exponent; ++i)
+		result *= base;
+
+	return result;
+}
+*/
+
+// 进阶解法: 使用斐波那契数列减少乘方次数
+double PowerWithUnsignedExponent(double base, unsigned int exponent)
+{
+	if (exponent == 0)
+		return 1;
+	if (exponent == 1)
+		return base;
+
+	double result = PowerWithUnsignedExponent(base, exponent >> 1);
+	result *= result;
+	if ((exponent & 0x1) == 1)
 		result *= base;
 
 	return result;
@@ -56,5 +75,5 @@ double PowerWithUnsignedExponent(double base, unsigned int exponent)
 
 int main(int argc, char* argv)
 {
-	cout << Power(5, -5) << endl;
+	cout << Power(2, -1) << endl;
 }
