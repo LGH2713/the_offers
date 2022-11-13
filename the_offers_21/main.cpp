e4c2f0f2ea55266585b6e134155365320b8f53f0
lgh <2524398864@qq.com>
2022-11-13 15:23:32 +0800

    feat: 补充拓展解法

--- the_offers_21/main.cpp
+++ the_offers_21/main.cpp
@@ -9,7 +9,7 @@
 #include<exception>
 using namespace std;
 
-void ReorderOddEvent(int* pData, unsigned int length)
+void ReorderOddEvent_1(int* pData, unsigned int length)
 {
 	if (pData == nullptr || length == 0)
 		return;
@@ -37,12 +37,54 @@ void ReorderOddEvent(int* pData, unsigned int length)
 	}
 }
 
+void Reorder(int* pData, unsigned int length, bool(*func)(int))
+{
+	if (pData == nullptr || length == 0)
+		return;
+
+	int* pBegin = pData;
+	int* pEnd = pData + length - 1;
+
+	while (pBegin < pEnd)
+	{
+		while (pBegin < pEnd && !func(*pBegin))
+			pBegin++;
+
+		while (pBegin < pEnd && func(*pEnd))
+			pEnd--;
+
+		if (pBegin < pEnd)
+		{
+			int temp = *pBegin;
+			*pBegin = *pEnd;
+			*pEnd = temp;
+		}
+	}
+}
+
+bool isEvent(int n)
+{
+	return (n & 1) == 0;
+}
+
+void ReorderOddEvent_2(int* pData, unsigned int length)
+{
+	Reorder(pData, length, isEvent);
+}
+
 
 int main(int argc, char* argv)
 {
-	int array[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	ReorderOddEvent(array, 10);
+	int array_1[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	ReorderOddEvent_1(array_1, 10);
 
 	for (int i = 0; i < 10; i++)
-		cout << array[i] << " ";
+		cout << array_1[i] << " ";
+
+	cout << endl;
+
+	int array_2[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	ReorderOddEvent_2(array_2, 10);
+	for (int i = 0; i < 10; i++)
+		cout << array_2[i] << " ";
 }
